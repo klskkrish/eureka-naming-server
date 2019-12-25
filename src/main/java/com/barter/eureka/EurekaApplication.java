67d8be89c14fed467c099d764832affed6fff7d0
Suresh Krishantha <klskkrish@gmail.com>
2019-12-25 17:07:27 +0530

    eureka client reg security issue fixed

--- src/main/java/com/barter/eureka/EurekaApplication.java
+++ src/main/java/com/barter/eureka/EurekaApplication.java
@@ -1,17 +1,14 @@
 package com.barter.eureka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-@EnableEurekaServer
+
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
-
 }
--- src/main/java/com/barter/eureka/security/SpringWebSecurity.java
+++ src/main/java/com/barter/eureka/security/SpringWebSecurity.java
@@ -4,11 +4,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * @author sureshk
+ *
+ */
 @EnableWebSecurity
 public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
-	
+
+	/* (non-Javadoc)
+	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		
+		//eureka client register csrf false
+		http.httpBasic().and().authorizeRequests().antMatchers("/eureka/apps", "/eureka/apps/**").permitAll().anyRequest()
+				.authenticated().and().csrf().disable();
+		//authenticate all calls
+		// http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
 }
